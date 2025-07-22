6008e6ec6c87e0bb3c4722f7bb52127de99a18ad
OmeshSachethana <sachethana12@gmail.com>
2025-07-22 13:48:33 +0530

    added new files

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-export { default as About } from './About';
-export { default as Expertise } from './Expertise';
-export { default as ContactUs } from './ContactUs';
-export { default as Home } from './Home';
-export { default as Packages } from './Packages';
-export { default as TravelerAddPage } from './TravelerAddPage';
